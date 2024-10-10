88f0d8be6f3b27f4f9f81009fa17d35b43a86a6a
paulaminguez14 <dominguez.bernabe.paula@iescamas.es>
2024-10-10 09:21:24 +0200

    con errores

--- Prueba.php
+++ Prueba.php
@@ -1,7 +1,12 @@
 <?php
     require_once 'Cliente.php';
+    require_once 'ProductoFisico.php';
     $c1 = new Cliente("Paula Domínguez");
     $c2 = new Cliente("Rocío González");
+
+    $productofisico = new ProductoFisico("Telefono", precio: 1000, 0.5);
+    echo "Nombre: " . $productofisico->getNombre() . ", Precio: " . $productofisico->getPrecio() . ", Peso: " . $productofisico->calculaPrecioFinal();
+
 ?>
 
 <ol>
